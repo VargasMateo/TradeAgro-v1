ea823de4cc9644fa570f629096eaf106ada9f3d5
vlDevelopment <105453278+VargasMateo@users.noreply.github.com>
2026-03-20 13:01:26 -0300

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap, ZoomControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { useEffect } from 'react';
 
 // Fix for default marker icon
 delete (L.Icon.Default.prototype as any)._getIconUrl;
@@ -21,13 +22,41 @@ interface MapProps {
   }>;
 }
 
+const ZoomHandler = () => {
+  const map = useMap();
+  useEffect(() => {
+    map.zoomControl?.remove();
+  }, [map]);
+  return null;
+};
+
+const FitBounds = ({ markers }: { markers: any[] }) => {
+  const map = useMap();
+  useEffect(() => {
+    if (markers && markers.length > 0) {
+      const bounds = L.latLngBounds(markers.map(m => m.position));
+      map.fitBounds(bounds, { padding: [50, 50], maxZoom: 15 });
+    }
+  }, [markers, map]);
+  return null;
+};
+
 const Map = ({ center = [-31.4201, -64.1888], popupContent, markers }: MapProps) => {
   return (
-    <MapContainer center={center} zoom={5} scrollWheelZoom={false} style={{ height: '100%', width: '100%', borderRadius: '1rem' }}>
+    <MapContainer
+      center={center}
+      zoom={13}
+      scrollWheelZoom={true}
+      zoomControl={false}
+      style={{ height: '100%', width: '100%', borderRadius: '1rem' }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <ZoomControl position="bottomleft" />
+      <ZoomHandler />
+      {markers && <FitBounds markers={markers} />}
       {markers ? (
         markers.map((marker, index) => (
           <Marker key={index} position={marker.position}>
