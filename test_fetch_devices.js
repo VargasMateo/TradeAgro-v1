2add792f10ca16bee87a576ed87762dec37ab8b9
vlDevelopment <105453278+VargasMateo@users.noreply.github.com>
2026-07-27 14:48:25 -0300

    sobras

--- test_fetch_devices.js
+++
@@ -1,6 +0,0 @@
-async function test() {
-  const res = await fetch('http://localhost:3000/backend/weather-stations/devices');
-  const json = await res.json();
-  console.log(JSON.stringify(json.data.slice(0,2), null, 2));
-}
-test();
--- test_mkl_devices.ts
+++
@@ -1,9 +0,0 @@
-import { getMklToken } from './server.ts';
-
-async function test() {
-  const token = await getMklToken();
-  const res = await fetch('https://panel.mklagro.com/api/device', { headers: { token } });
-  const json = await res.json();
-  console.log(JSON.stringify(json.data.slice(0, 3), null, 2));
-}
-test();
